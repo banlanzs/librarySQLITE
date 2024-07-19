36259c6172d4900a157a34080e56d95ae7e98187
banlanzs <147498418+banlanzs@users.noreply.github.com>
2024-07-19 18:38:12 +0800

    周爽

--- readme.h
+++ readme.h
@@ -1,1 +1,25 @@
-基于QT C++ SQLITE 图书管理系统
+#ifndef README_H
+#define README_H
+//zhoushuang
+#include <QDialog>
+#include<QDebug>
+namespace Ui {
+class readme;
+}
+
+class readme : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit readme(QWidget *parent = nullptr);
+    ~readme();
+
+private slots:
+    void on_confirmButton_clicked();
+
+private:
+    Ui::readme *ui;
+};
+
+#endif // README_H
